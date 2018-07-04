ab027ec0a0e8b295c576c7dce9673f1abdada085
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-04 23:14:00 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704230020) do
+ActiveRecord::Schema.define(version: 20180704231154) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.decimal "balance"
   end
