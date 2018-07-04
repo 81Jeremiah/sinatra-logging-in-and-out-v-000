cd982ad6cfb7ffd78f65447885d0dfdf1690e9b4
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-04 23:07:52 +0000

    automatically backed up by learn

--- db/migrate/20180704230020_create_users.rb
+++ db/migrate/20180704230020_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.sting :name
+      t.string :name
       t.string :email
       t.decimal :balance
     end
